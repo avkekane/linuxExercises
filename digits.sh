bcddb284b00a0f4a691ab7222482bb32c74f6d91
Avanti <avkekane@wisc.edu>
2026-03-03 09:10:05 -0600

    digits.sh

--- digits.sh
+++ digits.sh
@@ -1,3 +1,21 @@
- #!/bin/bash 
 
+#!/bin/bash
+
+# Initialize the sum variable
+sum=0
+
+# Traverse a sequence using brace expansion {1000..2000}
+for num in {1000..2000}; do
+    # Conditional statement using a regular expression
+    # Checks if the number contains only 0s and 1s
+    if [[ $num =~ ^[01]+$ ]]; then
+        # Update the sum using the x=$((...)) syntax from your reference
+        sum=$((sum + num))
+    fi
+done
+
+# Output the final result
+echo "The sum is: $sum"
+echo "Sum of numbers with digits in set [1,0] is $sum"
+	
 

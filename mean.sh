464273a74da1de9bb51e47dc2f74df4d53d04c0f
Linyan Li <lli643@wisc.edu>
2026-03-05 10:31:31 -0600

    Finish mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,18 @@
- #!/bin/bash
+#!/bin/bash
+
+# check arguments
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "usage: $0 <column> [file.csv]" 1>&2
+    exit 1
+fi
+
+col="$1"
+
+# if file provided use it, otherwise read stdin
+if [ $# -eq 2 ]; then
+    input="$2"
+else
+    input="/dev/stdin"
+fi
+
+cut -d',' -f"$col" "$input" | tail -n +2 | awk '{sum+=$1; n++} END{if(n>0) print sum/n}'

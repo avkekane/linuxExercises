626a7d260afdf2f91f22bd2c2103c1c2bff24c89
Linyan Li <lli643@wisc.edu>
2026-03-05 10:29:02 -0600

    Finish rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,13 @@
 #!/bin/bash 
+
+# Check number of arguments
+if [ $# -ne 2 ]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+
+dir="$1"
+n="$2"
+
+# Find files larger than n bytes and remove them
+find "$dir" -type f -size +"${n}"c -print -delete
